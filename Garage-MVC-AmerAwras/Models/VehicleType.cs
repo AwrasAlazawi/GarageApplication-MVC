a35a4135b45817332f05a1264b809f5618d88227
AwrasAlazawi <awrasabdulsattar@hotmail.com>
2018-05-21 13:28:01 +0200

    finished the Model

---
+++ Garage-MVC-AmerAwras/Models/VehicleType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garage_MVC_AmerAwras.Models
+{
+
+    public enum VehicleType
+    {
+        Car,
+        Bus,
+        Boat,
+        Motorcycle,
+        Airplane
+    }
+}
