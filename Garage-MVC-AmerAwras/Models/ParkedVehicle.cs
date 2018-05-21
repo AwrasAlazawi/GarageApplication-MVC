fa7a44b36e5db12362e0bd018ee47e43a004d921
AwrasAlazawi <awrasabdulsattar@hotmail.com>
2018-05-21 11:12:32 +0200

    Add project files.

---
+++ Garage-MVC-AmerAwras/Models/ParkedVehicle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garage_MVC_AmerAwras.Models
+{
+    public class ParkedVehicle
+    {
+
+
+    }
+}
